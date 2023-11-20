5a8c3986797db7e734fce2995bbadf2a72c5c37f
Nithin Pranav <nithinpranav12@gmail.com>
2023-11-20 19:31:57 +0530

    Final Code
    BidHub

---
+++ server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+import cors from "cors";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import path from 'path';
+
+dotenv.config();
+connectDB();
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname,'./client/build')))
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
+
+app.use('*',function(res,req){
+  res.sendFile(path.join(__dirname, './client/build/index.html'))
+});
+
+const PORT = process.env.PORT || 3005;
+
+app.listen(PORT, () => {
+  console.log(
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+      .white
+  );
+});
